24a4fab21b198a850df078625a37e15d7e146234
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-03 16:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+const imgs = document.querySelectorAll("img");
+let imgArr = Array.from(imgs);
+let randomPhotoAdded = imgArr[Math.round(Math.random()*5)];
+imgArr.push(randomPhotoAdded);
